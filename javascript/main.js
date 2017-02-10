ff4bd7868fe9e60603938ded5e7b3f8670ec66c7
Joshua Bradford <jjyet@yahoo.com>
2017-02-10 11:35:19 -0500

    added text underneath the searched images

--- javascript/main.js
+++ javascript/main.js
@@ -2,8 +2,31 @@ var currentHtml;
 
 for(var count = 0; count < etsyData.results.length; count++){
   currentHtml = $(".results").html();
-  console.log(currentHtml);
-  currentHtml += `<div class="image"><img src="${etsyData.results[count].Images[0].url_170x135}"/></div>`;
+  currentHtml += `<div class="image-container">
+  <div class="image"><img width=230 height=184 src="${etsyData.results[count].Images[0].url_170x135}"/></div>
+  <div class="cap-sell-price">
+  <div class="caption">${captionShrink(etsyData.results[count].title)}</div>
+  <div class="seller">${etsyData.results[count].Shop.shop_name}</div>
+  <div class="p-code">${convertPriceCode(etsyData.results[count].currency_code)}</div>
+  <div class="price">${etsyData.results[count].price}</div>
+  </div>
+  <div>`;
   $(".results").html(currentHtml);
-  console.log(currentHtml);
+}
+
+function captionShrink(longCaption){
+  if(longCaption.length > 34){
+    return longCaption.slice(0, 31) + "...";
+  }
+}
+
+function convertPriceCode(pCode){
+  if(pCode === "USD"){
+    return "$";
+  }else if(pCode === "EUR"){
+    return "&#8364;";
+  }else if(pCode === "GBP"){
+    return "&#163;";
+  }
+
 }
