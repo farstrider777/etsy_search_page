f5cf0a047db0e210c4b0736b7406e3aeeec503ea
Joshua Bradford <jjyet@yahoo.com>
2017-02-12 17:03:56 -0500

    Got heart and hamburger working properly by using currenttarget.children :)

--- javascript/main.js
+++ javascript/main.js
@@ -5,28 +5,32 @@ for(var count = 0; count < 16; count++){
   currentHtml = $(".results").html();
   currentHtml += `<div class="image-container">
   <div class="heart-burger">
-    <div class="heart-container"><img class="invisible" src="images/heart.png"></div>
-    <div class="burger-container"><img class="invisible" src="images/hamburger.png" ></div>
+    <div class="heart-container"><a href="#"><img class="invisible" src="images/heart.png"></a></div>
+    <div class="burger-container"><a href="#"><img class="invisible" src="images/hamburger.png"></a></div>
   </div>
   <div class="image"><a href="${etsyData.results[count].url}">
-    <img class="search-image" src="${etsyData.results[count].Images[0].url_170x135}"/></div>
-  </a>
+    <img class="search-image" src="${etsyData.results[count].Images[0].url_170x135}"/></a>
+  </div>
   <div class="caption"><a href="${etsyData.results[count].url}">
-    ${etsyData.results[count].title}</div>
-  </a>
+    ${etsyData.results[count].title}</a>
+  </div>
   <div class="cap-sell-price">
     <div class="seller"><a href="${etsyData.results[count].Shop.url}">
-      ${etsyData.results[count].Shop.shop_name}</div>
-    </a>
+      ${etsyData.results[count].Shop.shop_name}</a>
+    </div>
     <div class="price"><a href="#">
-      ${etsyData.results[count].price}</div>
-    </a>
+      ${etsyData.results[count].price}</a>
+    </div>
     <div class="p-code"><a href="#">
-      ${convertPriceCode(etsyData.results[count].currency_code)}</div>
-    </a>
+      ${convertPriceCode(etsyData.results[count].currency_code)}</a>
+    </div>
   </div>`;
   $(".results").html(currentHtml);
 }
+
+//${if(count < 4)}
+//<div class="ad">Ad</div> 
+
 
 function convertPriceCode(pCode){
   if(pCode === "USD"){
@@ -39,14 +43,17 @@ function convertPriceCode(pCode){
 }
 
 $(".image-container").hover(function(event) {
-  console.log(event.currentTarget);
   var jevent = event.currentTarget;
   jevent = $(jevent);
-  console.log(jevent);
+  console.log(jevent.children('div.heart-burger'));
+  jevent.children("div").children("div").children("a").children('img').removeClass("invisible").addClass("reg-size");
 //  $(jevent).find("div").removeClass("invisible").addClass("reg-size");
-  $(".heart-burger img").removeClass("invisible").addClass("reg-size");
-  $(".heart-container").addClass("hb-background");
-  $(".burger-container").addClass("hb-background");
+  //$(".heart-burger img").removeClass("invisible").addClass("reg-size");
+  jevent.children("div").children("div.heart-container").addClass("hb-background");
+  jevent.children("div").children("div.burger-container").addClass("hb-background");
+  //$(".heart-container").addClass("hb-background");
+  //$(".burger-container").addClass("hb-background");
+  //jevent.addClass("z-up");
 }, function(event) {
   $(".heart-burger img").removeClass("reg-size").addClass("invisible");
   $(".heart-container").removeClass("hb-background");
